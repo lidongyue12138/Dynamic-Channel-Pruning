8993d922756d928bbb43266ff6b7627f10f3d2b0
Oliver <lidongyue12138@gmail.com>
2019-06-04 20:57:30 +0800

    Update CifarNet.py

--- CifarNet.py
+++ CifarNet.py
@@ -15,7 +15,7 @@ class CifarNet:
         self.build_model()
         self.sess.run(self.init)
 
-    def train_model(self, batch_X, batch_y, pruning_rate=1):
+    def train_model(self, batch_X, batch_y, pruning_rate=[1]):
         feed_dict = {
             self.xs: batch_X,
             self.ys_orig: batch_y,
@@ -28,7 +28,7 @@ class CifarNet:
         loss = self.sess.run(self.cross_entropy, feed_dict=feed_dict)
         return loss
     
-    def test_acc(self, X, y, pruning_rate = 1.0):
+    def test_acc(self, X, y, pruning_rate = [1.0]):
         feed_dict = {
             self.xs: X,
             self.ys_orig: y,
